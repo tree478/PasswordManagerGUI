05fe067672ab6ff618f0403929a8ca12e86d3fa2
tree478 <ssshel25@students.shrewsbury.k12.ma.us>
2023-06-04 14:55:52 -0400

    Finished Genius Hour Project!

--- ctk.py
+++ ctk.py
@@ -4,7 +4,8 @@ from tkinter import ttk
 from typing import Optional, Tuple, Union
 import customtkinter as ctk
 from cryptography.fernet import Fernet
-import json 
+import json
+import webbrowser
 
 class PasswordManager(ctk.CTk):
 
@@ -27,11 +28,6 @@ class PasswordManager(ctk.CTk):
 
         self.entry_frame = ctk.CTkFrame(self, width=1200, height=600)
         self.entry_frame.grid(row=0, column=0, sticky='nsew')
-
-        # self.entry_frame.grid_rowconfigure(0, weight=1)
-        # self.entry_frame.grid_rowconfigure(4, weight=1)
-        # self.entry_frame.grid_columnconfigure(0, weight=1)
-        # self.entry_frame.grid_columnconfigure(2, weight=1)
 
         self.instructions = ctk.CTkLabel(self.entry_frame, text = "Please enter your master password:")
         self.instructions.grid(row=0, column=0, sticky='NEWS', padx=500, pady=(250,10))
@@ -57,9 +53,6 @@ class PasswordManager(ctk.CTk):
         self.sidebar_button_2 = ctk.CTkButton(self.sidebar_frame, text='Add Password', command=self.change_to_add_password)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
 
-        self.sidebar_button_3 = ctk.CTkButton(self.sidebar_frame)
-        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
-
         self.mainpage = ctk.CTkFrame(self.interface, width=1000, height=600)
         self.mainpage.pack(side='left', fill='both')
         
@@ -67,6 +60,10 @@ class PasswordManager(ctk.CTk):
         self.welcome_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         self.show_frame(self.entry_frame)
+
+        self.dict_of_buttons = {}
+
+        self.bind('<Return>', lambda event: self.check_password())
 
     def show_frame(self, frame):
         frame.tkraise()
@@ -90,16 +87,19 @@ class PasswordManager(ctk.CTk):
     
     def write_dict(self, dict):
         with open("data.py", "a") as file:
-            #json.dump(dict, file)
             json.dump(dict,file)
 
     def read_list(self):
         with open('data.py', 'rb') as file:
             n_list = json.load(file)
             return n_list
+        
+    def open_link(self, link):
+        print(link)
+        chrome = webbrowser.Chrome(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+        chrome.open(link)
 
     def store_input(self): #when you press submit on the add passwords frame, this function runs, it stores the user input in data.py
-
         self.info = {}
 
         self.account_name_input = str(self.fer.encrypt(self.account_name_entry.get().encode()))
@@ -110,15 +110,20 @@ class PasswordManager(ctk.CTk):
         self.info['password'] = self.password_input
         self.notes_input = str(self.fer.encrypt(self.notes_entry.get().encode()))
         self.info['notes'] = self.notes_input
+        self.link_input = str(self.fer.encrypt(self.link_entry.get().encode()))
+        self.info['link'] = self.link_input
 
         self.write_dict(self.info)
 
-    def show_account(self, account):
+        self.account_name_entry.delete(0, END)
+        self.username_entry.delete(0, END)
+        self.password_entry.delete(0, END)
+        self.link_entry.delete(0, END)
+        self.notes_entry.delete(0, END)
 
-        #print("this is the # that gets passed to the function", line_no)
+    def show_account(self, account, number):
 
         with open("data.py", "r") as f:
-            #print("this is the # that gets used for the indicies", line_no-1)
             list_of_accounts = []
             lines = f.readlines()
             for line in lines:
@@ -126,10 +131,7 @@ class PasswordManager(ctk.CTk):
                 value = line['account_name']
                 list_of_accounts.append(str(self.fer.decrypt(bytes(value[2:-1], 'utf-8'))))
             
-            numbers = dict(enumerate(list_of_accounts))
-            data = lines[list(numbers.keys())[list(numbers.values()).index(self.account)]]
-            print(list(numbers.keys())[list(numbers.values()).index(self.account)])
-            data = json.loads(data)
+        data = json.loads(lines[number-1])
 
         for self.widget in self.mainpage.winfo_children():
             self.widget.destroy()
@@ -152,11 +154,24 @@ class PasswordManager(ctk.CTk):
         self.password = ctk.CTkLabel(self.password_frame, text="Password: " + str(self.fer.decrypt(bytes(data['password'][2:-1], 'utf-8')))[2:-1])
         self.password.grid(row=0, column=0, padx=30, pady=10)
 
+        self.link_frame = ctk.CTkFrame(self.mainpage, width=600, height=40)
+        self.link_frame.grid(row=4, column=0, padx=200, pady=5)
+
+        self.link = ctk.CTkButton(self.link_frame, text="Link: " + str(self.fer.decrypt(bytes(data['link'][2:-1], 'utf-8')))[2:-1], command= lambda: self.open_link(str(self.fer.decrypt(bytes(data['link'][2:-1], 'utf-8')))[2:-1]))
+        self.link.grid(row=0, column=0, padx=30, pady=10)
+
         self.notes_frame = ctk.CTkFrame(self.mainpage, width=600, height=40)
         self.notes_frame.grid(row=3, column=0, padx=200, pady=5)
 
         self.notes = ctk.CTkLabel(self.notes_frame, text="Notes: " + str(self.fer.decrypt(bytes(data['notes'][2:-1], 'utf-8')))[2:-1])
         self.notes.grid(row=0, column=0, padx=30, pady=10)
+
+        self.mainpage.configure(width=1000, height=600)
+        self.account_name_frame.configure(width=600, height=40)
+        self.username_frame.configure(width=600, height=40)
+        self.password_frame.configure(width=600, height=40)
+        self.link_frame.configure(width=600, height=40)
+        self.notes_frame.configure(width=600, height=40)
 
 
     def change_to_add_password(self): #this changes the frame to the add passwords form
@@ -191,16 +206,20 @@ class PasswordManager(ctk.CTk):
         self.password_entry = ctk.CTkEntry(self.add_info_frame, placeholder_text="Password")
         self.password_entry.grid(row=4, column=2, sticky='news', pady=20)
 
+        self.link_label = ctk.CTkLabel(self.add_info_frame, text="Link:")
+        self.link_label.grid(row=5, column=1, sticky='news', padx=(350,20), pady=20)
+
+        self.link_entry = ctk.CTkEntry(self.add_info_frame, placeholder_text="Link")
+        self.link_entry.grid(row=5, column=2, sticky='news', pady=20)
+
         self.notes_label = ctk.CTkLabel(self.add_info_frame, text="Notes:")
-        self.notes_label.grid(row=5, column=1, sticky='news', padx=(350,20), pady=20)
+        self.notes_label.grid(row=6, column=1, sticky='news', padx=(350,20), pady=20)
 
         self.notes_entry = ctk.CTkEntry(self.add_info_frame, placeholder_text="Notes", height=50)
-        self.notes_entry.grid(row=5, column=2, sticky='news', pady=20)
+        self.notes_entry.grid(row=6, column=2, sticky='news', pady=20)
 
         self.submit_info = ctk.CTkButton(self.add_info_frame, text="Submit", command=self.store_input)
-        self.submit_info.grid(row=6, column=2, padx=20, pady=20)
-
-        print(self.submit_info)
+        self.submit_info.grid(row=7, column=2, padx=20, pady=20)
 
         self.add_password_instructions_frame.configure(width=1000, height=100)
         self.add_password_instructions_frame.grid_propagate(False)
@@ -208,8 +227,15 @@ class PasswordManager(ctk.CTk):
         self.add_info_frame.configure(width=1000, height=500)
         self.add_info_frame.grid_propagate(False)
 
+        self.bind('<Return>', lambda event: self.store_input())
+
         self.mainpage.configure(width=1000, height=600)
         self.mainpage.grid_propagate(False)
+
+    def create_button(self, account, number):
+        account_button = ctk.CTkButton(self.mainpage, text=account[2:-1], width=980, height=30, command=lambda: self.show_account(account, number))
+        self.dict_of_buttons[(account, number)] = account_button
+        return account_button
 
 
     def change_to_view_passwords(self): #the fucntion to let you view passwords, runs when you press the 'view passwords button'
@@ -225,23 +251,15 @@ class PasswordManager(ctk.CTk):
                 list_of_accounts.append(str(self.fer.decrypt(bytes(value[2:-1], 'utf-8'))))
             
             row_=0
-            column_=0
-            dict_of_buttons = {}
-            #numbers = dict(enumerate(list_of_accounts))
-            #list(numbers.keys())[list(numbers.values()).index(self.account)])
-            #list_of_accounts = dict(list_of_accounts)
+            self.number = 1
             for self.account in list_of_accounts:
-                dict_of_buttons[self.account] = ctk.CTkButton(self.mainpage, text=self.account[2:-1], command=lambda: self.show_account(self.account)).grid(row=row_, column=column_, padx=50, pady=10)
-                # print(list_of_accounts[i])
-                # list_of_accounts[self.account].grid(row=row_, column=column_, padx=50, pady=10)
-                #ctk.CTkButton(self.mainpage, text=self.account[2:-1], command=lambda: show_account(self.account)).grid(row=row_, column=column_, padx=50, pady=10)
-                #list_of_buttons.append(self.account)
-                print(dict_of_buttons)
-                self.mainpage.configure(width=1000, height=600)
-                self.mainpage.grid_propagate(False)
+                self.create_button(self.account, self.number).grid(row=row_, column=0, padx=10, pady=15)
                 row_ += 1
+                self.number += 1
+            self.mainpage.configure(width=1000, height=600)
+            self.mainpage.grid_propagate(False)
+                
             line_no += 1
-            #print(list_of_buttons)
 
 if __name__ == '__main__':
     app = PasswordManager()
